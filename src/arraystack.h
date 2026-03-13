d2c28eda64d75e48757ec59481cc33e3628a4839
RiceCell <russel.me789@gmail.com>
2026-03-13 12:29:13 +0800

    final final array stack

--- src/arraystack.h
+++ src/arraystack.h
@@ -1,4 +1,6 @@
+
 #pragma once
+#include <iostream>
 #include "array.h"
 #include "list.h"
 
@@ -8,31 +10,38 @@ class ArrayStack : public List<T>
 private:
     array<T> arr;
     size_t n;
+    size_t resize_count = 0;
 
     void resize()
     {
-        size_t newCap = (n == 0) ? 1 : (n < 100 ? n + 1 : 2 * n);
+        resize_count++;
+
+        size_t newCap = (n == 0) ? 1 : 2 * n;
         array<T> b(newCap);
         for (size_t i = 0; i < n; i++)
             b[i] = arr[i];
+        for (size_t i = n; i < newCap; i++)
+            b[i] = T();
+
         arr = b;
     }
 
 public:
     ArrayStack(size_t capacity = 1) : arr(capacity), n(0) {}
 
-    size_t size() const override
-    {
-        return n;
-    }
+    size_t size() const override { return n; }
 
     T get(const size_t i) const override
     {
+        if (i >= n)
+            return T();
         return arr[i];
     }
 
     T set(const size_t i, const T &x) override
     {
+        if (i >= n)
+            return T();
         T y = arr[i];
         arr[i] = x;
         return y;
@@ -42,8 +51,12 @@ public:
     {
         if (n + 1 > arr.length)
             resize();
-        for (size_t j = n; j > i; j--)
-            arr[j] = arr[j - 1];
+        if (i < n)
+        {
+            for (size_t j = n; j > i; j--)
+                arr[j] = arr[j - 1];
+        }
+
         arr[i] = x;
         n++;
     }
@@ -51,16 +64,14 @@ public:
     T remove(const size_t i) override
     {
         T removed = arr[i];
-
         for (size_t j = i; j < n - 1; j++)
             arr[j] = arr[j + 1];
 
         arr[n - 1] = T();
 
         n--;
-        if (arr.length >= 2 * n && n > 0)
+        if (arr.length >= 3 * n && n > 0)
             resize();
-
         return removed;
     }
 };
