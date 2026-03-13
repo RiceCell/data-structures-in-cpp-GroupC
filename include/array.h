edcc7480c71c93f795cffd2f93306038ac2cfb7d
RiceCell <russel.me789@gmail.com>
2026-03-13 11:04:42 +0800

    added array.h and arraystack.h

---
+++ include/array.h
@@ -0,0 +1,42 @@
+// array wrapper
+#include <cassert>
+#include <cstddef>
+
+template <typename T>
+struct array
+{
+    array(size_t n)
+    {
+        a = new T[n];
+        length = n;
+    }
+
+    ~array()
+    {
+        delete[] a;
+    }
+
+    T &operator[](size_t i) const
+    {
+        assert(i < length);
+        return a[i];
+    }
+
+    array(const array<T> &) = delete;
+
+    array<T> &operator=(array<T> &b)
+    {
+        if (a != nullptr)
+        {
+            delete[] a;
+        }
+        a = b.a;
+        b.a = nullptr;
+        length = b.length;
+        b.length = 0;
+        return *this;
+    }
+
+    size_t length;
+    T *a;
+};
