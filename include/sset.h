e94eec7b3fdce9453d6aa3b6240c71db40d65e1e
Angelo Manlangit <manlangitangelomh@gmail.com>
2026-03-18 22:49:27 +0800

    Transferred skipnode to sset.h and made rbt node

---
+++ include/sset.h
@@ -0,0 +1,41 @@
+// NODE FOR SKIPLISTS
+template <typename T>
+class SkipNode {
+    public:
+        T data;
+        size_t height;
+        SkipNode **next;
+
+        // for sentinel
+        SkipNode(size_t height) : height(height) {
+            next = new SkipNode*[height];
+
+            for (size_t i = 0; i < height; i++) {
+                next[i] = nullptr;
+            }
+        }
+
+        SkipNode(T x, size_t height) : data(x), height(height) {
+            next = new SkipNode*[height];
+
+            for (size_t i = 0; i < height; i++) {
+                next[i] = nullptr;
+            }
+        }
+
+        ~SkipNode() {
+            delete[] next;
+        }
+};
+
+// Red-Black Tree Node
+template <typename T>
+struct RBTNode {
+    T data;
+    bool isBlack;
+    RBTNode<T> *left;
+    RBTNode<T> *right;
+    RBTNode<T> *parent;
+
+    RBTNode(const T &x, bool isB = false) : data(x), isBlack(isB) {};
+};
